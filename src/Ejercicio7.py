04e2a4e41678f6e64b9c6f8771c13b1c76148625
Fco. José Macho Delgado <fmacdel478@g.educaand.es>
2023-10-17 20:56:55 +0200

    4

---
+++ src/Ejercicio7.py
@@ -0,0 +1,14 @@
+#Escribir un programa que muestre por pantalla la tabla de multiplicar del 1 al 10.
+
+def tabla110():
+    tabla = ""
+    for i in range(1,11):
+        for j in range(1,11):
+            resultado = i * j
+            tabla += f"{i} x {j} = {resultado}\n"
+    return tabla
+
+
+if __name__ == "__main__":
+    resultado = tabla110()
+    print(resultado)
