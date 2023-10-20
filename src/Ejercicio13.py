ee48937902d44e761f17de6db8db588139fcb277
Fco Jose Macho Delgado <fjmac@iesrafaelalberti.es>
2023-10-20 12:01:45 +0200

    9

--- src/Ejercicio13.py
+++ src/Ejercicio13.py
@@ -1,10 +1,12 @@
 #Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el usuario escriba “salir” que terminará.
 
-def eco():
-    while True:
-        frase = input("-->")
-        if frase == "salir":
-            break
-        print(frase)
+def eco(entrada):
+   return entrada == "salir"
 
-eco()
+if __name__ == "__main__":
+   entrada = input("Escriba algo (escriba 'salir' para salir): ")
+
+   while not eco(entrada):
+      print(entrada)
+
+      entrada = input("Escriba algo (escriba 'salir' para salir): ")
